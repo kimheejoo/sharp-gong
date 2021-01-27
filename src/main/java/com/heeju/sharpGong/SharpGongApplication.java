246fcb190b010534489b851e17b02cdeb5fb6e04
kimheejoo <h32j00@gmail.com>
2021-01-28 00:06:21 +0900

    start project

---
+++ src/main/java/com/heeju/sharpGong/SharpGongApplication.java
@@ -0,0 +1,13 @@
+package com.heeju.sharpGong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharpGongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharpGongApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/heeju/sharpGong/SharpGongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heeju.sharpGong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharpGongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
