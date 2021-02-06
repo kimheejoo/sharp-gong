16802a40a031256c993894ad6bf1cf0e5987455a
d0hyeon <ehgus6887@gmail.com>
2021-02-07 01:21:41 +0900

    feat: 공용 레이아웃 컴포넌트 추가

---
+++ src/front/src/components/common/styles.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const H1 = styled.div`
+  padding: 10px;
+  font-size: 28px;
+  border-bottom: 1px solid #333;
+`;
+
+export const Content = styled.div`
+  margin-top: 20px;  
+`;
+
+export const Layout = styled.div`
+  width: 1200px;
+  margin: 30px auto;
+`;
