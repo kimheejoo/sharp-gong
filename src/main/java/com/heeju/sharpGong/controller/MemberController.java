e3cd850ef989cec2f39f850dec50d5a1e49e279f
kimheejoo <h32j00@gmail.com>
2021-02-06 23:53:53 +0900

    post login

--- src/main/java/com/heeju/sharpGong/controller/MemberController.java
+++ src/main/java/com/heeju/sharpGong/controller/MemberController.java
@@ -7,16 +7,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.naming.Binding;
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class MemberController {
 
     private final MemberService memberService;
@@ -26,7 +27,7 @@ public class MemberController {
         model.addAttribute("loginForm", new LoginForm());
         return "/login";
     }
-
+/*
     @PostMapping("/login")
     public String login(@Valid LoginForm form, BindingResult result, Model model) {
         if (result.hasErrors()) {
@@ -41,6 +42,12 @@ public class MemberController {
             return "/login";
         }
     }
+ */
+    @PostMapping("/login")
+    public @ResponseBody Member login(@RequestBody LoginForm form){
+        return memberService.Login(form);
+    }
+
     @GetMapping("/member/{memberId}/todo")
     public String userHome(@PathVariable("memberId") String memberId, Model model){
         model.addAttribute("data",memberId);
--- src/main/java/com/heeju/sharpGong/controller/MemberForm.java
+++ src/main/java/com/heeju/sharpGong/controller/MemberForm.java
@@ -3,15 +3,12 @@ package com.heeju.sharpGong.controller;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotEmpty;
 
 @Getter @Setter
 public class MemberForm {
 
-    @NotEmpty(message = "회원 아이디는 필수입니다.")
     private String memberId;
 
-    @NotEmpty(message = "회원 비밀번호는 필수입니다.")
     private String memberPassword;
 
     private String nickname;
